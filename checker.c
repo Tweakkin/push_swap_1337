1a85b6b2ff2f2c6a9893b0675405f57fcc9c2c44
Tweakkin <yahyaboukhmira7@gmail.com>
2025-12-22 16:33:46 +0100

    working on bonus

---
+++ checker.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   checker.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yboukhmi <yboukhmi@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/22 16:20:09 by yboukhmi          #+#    #+#             */
+/*   Updated: 2025/12/22 16:23:55 by yboukhmi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+void print_list(t_stack *head) {
+    t_stack *current = head;
+    while (current != NULL) {
+        printf("%d -> ", current->value);
+        current = current->next;
+    }
+    printf("NULL\n");
+}
+
+int main(int argc, char **argv)
+{
+	t_data *a;
+	t_data *b;
+	
+	if (argc < 2)
+		return 0;
+	init_data(&a, &b);
+	args_to_arr(a, b, argc, argv);
+	index_stack(a->stack);
+	print_list(a->stack);
+	return (0);
+}
