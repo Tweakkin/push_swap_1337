3b6bd836320ead322bc4588a615cb9f467342da3
Tweakkin <yahyaboukhmira7@gmail.com>
2025-12-23 18:41:37 +0100

    done

--- operations.c
+++ operations.c
@@ -6,13 +6,13 @@
 /*   By: yboukhmi <yboukhmi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/14 21:35:23 by yboukhmi          #+#    #+#             */
-/*   Updated: 2025/12/14 23:46:04 by yboukhmi         ###   ########.fr       */
+/*   Updated: 2025/12/23 17:29:21 by yboukhmi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-int ss(t_data *a, t_data *b)
+int ss(t_data *a, t_data *b, int print)
 {
 	int	swapped;
 
@@ -28,11 +28,15 @@ int ss(t_data *a, t_data *b)
 		swapped = 1;
 	}
 	if (swapped == 1)
-		return (write(1, "ss\n", 3), 1);
+	{
+		if (print == 1)
+			write(1, "ss\n", 3);
+		return (1);
+	}
 	return (0);
 }
 
-int	rr(t_data *a, t_data *b)
+int	rr(t_data *a, t_data *b, int print)
 {
 	int	rotated;
 
@@ -48,11 +52,15 @@ int	rr(t_data *a, t_data *b)
 		rotated = 1;
 	}
 	if (rotated == 1)
-		return (write(1, "rr\n", 3), 1);
+	{
+		if (print == 1)
+			write(1, "rr\n", 3);
+		return (1);
+	}
 	return (0);
 }
 
-int	rrr(t_data *a, t_data *b)
+int	rrr(t_data *a, t_data *b, int print)
 {
 	int	rotated;
 
@@ -68,6 +76,10 @@ int	rrr(t_data *a, t_data *b)
 		rotated = 1;
 	}
 	if (rotated == 1)
-		return (write(1, "rrr\n", 4), 1);
+	{
+		if (print == 1)
+			write(1, "rrr\n", 4);
+		return (1);
+	}
 	return (0);
 }
